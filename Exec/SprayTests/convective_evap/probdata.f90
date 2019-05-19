e524e9478b09272d63e0f7ac0ee8e7806c489ca7
Marco Arienti <marient@sandia.gov>
2019-05-19 16:08:51 -0600

    First revision.

---
+++ Exec/SprayTests/convective_evap/probdata.f90
@@ -0,0 +1,8 @@
+module probdata_module
+
+  ! TG parameters
+  double precision, save :: reynolds, mach
+  double precision, save :: L_x, L_y, L_z
+  double precision, save :: u_ref, p_ref, T_ref
+
+end module probdata_module
