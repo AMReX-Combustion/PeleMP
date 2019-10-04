8b50e46fcc79b101ecf90cb8b3cc6b665003b1d1
Marco Arienti <marient@sandia.gov>
2019-10-03 20:15:11 -0400

    First revision - to run on summit with ASCENT/CONDUIT.

---
+++ Exec/NSCBC_flame/go_build.sh
@@ -0,0 +1,4 @@
+make -j USE_CONDUIT=TRUE \
+CONDUIT_DIR=/gpfs/alpine/proj-shared/csc340/larsen/ascent/uberenv_libs/spack/opt/spack/linux-rhel7-ppc64le/gcc-5.4.0/conduit-master-asp6z3xzt32za4lzx2oc4kdv2hynqmcr/
+export USE_ASCENT=TRUE \
+ASCENT_DIR=/gpfs/alpine/proj-shared/csc340/larsen/stats_no_cuda_shared/ascent/install
