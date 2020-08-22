717e4c9d200dcfd3ecba9862039a49cdbcdc78b9
Landon Owen <ldowen@sandia.gov>
2020-08-22 12:39:11 -0700

    Added new timing test with simple advecting particles

---
+++ Exec/SprayTests/timing_test/SprayParticlesInitInsert.cpp
@@ -0,0 +1,121 @@
+
+#include <SprayParticles.H>
+#include <AMReX_Particles.H>
+#include <PeleC.H>
+#include "prob.H"
+
+using namespace amrex;
+
+bool
+SprayParticleContainer::insertParticles(Real time,
+                                        Real dt,
+                                        int  nstep,
+                                        int  lev,
+                                        int  finest_level)
+{
+  return false;
+}
+
+bool
+SprayParticleContainer::injectParticles(Real time,
+                                        Real dt,
+                                        int  nstep,
+                                        int  lev,
+                                        int  finest_level)
+{
+  return false;
+}
+
+void
+SprayParticleContainer::InitSprayParticles(AmrLevel* pelec, const int& lev, const int& num_ppc)
+{
+  const int numGrids = pelec->numGrids();
+  Real part_rho = ProbParm::partRho;
+  Real part_dia = ProbParm::partDia;
+  Real T_ref = ProbParm::partTemp;
+  const IntVect num_part = ProbParm::partNum;
+  const auto dx = Geom(lev).CellSizeArray();
+  const auto plo = Geom(lev).ProbLoArray();
+  const auto phi = Geom(lev).ProbHiArray();
+  const Box& boxDom = Geom(lev).Domain();
+  const Real len = phi[0] - plo[0];
+  const RealVect dx_part(AMREX_D_DECL(len/Real(num_part[0]),
+				      len/Real(num_part[1]),
+				      len/Real(num_part[2])));
+  for (MFIter mfi = MakeMFIter(lev); mfi.isValid(); ++mfi) {
+    Box tile_box = mfi.tilebox();
+    tile_box &= boxDom;
+    const RealBox tile_realbox(tile_box, Geom(lev).CellSize(), Geom(lev).ProbLo());
+    Gpu::HostVector<ParticleType> host_particles;
+#ifdef USE_SPRAY_SOA
+    std::array<Gpu::HostVector<Real>, NAR_SPR > host_real_attribs;
+#endif
+    RealVect hi_end;
+    RealVect start_part;
+    RealVect part_loc;
+    for (int dir = 0; dir != AMREX_SPACEDIM; ++dir) {
+      Real box_length = tile_realbox.length(dir);
+      Real lo_end = tile_realbox.lo(dir);
+      hi_end[dir] = tile_realbox.hi(dir);
+      Real close_part = lo_end/dx_part[dir] - 0.5;
+      int part_n = std::floor(close_part);
+      start_part[dir] = (part_n + 1.5)*dx_part[dir];
+      part_loc[dir] = start_part[dir];
+    }
+    while (part_loc[0] < hi_end[0] + 1.E-12) {
+      part_loc[1] = start_part[1];
+      while (part_loc[1] < hi_end[1] + 1.E-12) {
+#if AMREX_SPACEDIM == 3
+	part_loc[2] = start_part[2];
+	while (part_loc[2] < hi_end[2] + 1.E-12) {
+#endif
+	  ParticleType p;
+	  p.id() = ParticleType::NextID();
+	  p.cpu() = ParallelDescriptor::MyProc();
+	  for (int dir = 0; dir != AMREX_SPACEDIM; ++dir) p.pos(dir) = part_loc[dir];
+#ifdef USE_SPRAY_SOA
+          for (int dir = 0; dir != AMREX_SPACEDIM; ++dir)
+            host_real_attribs[PeleC::pstateVel+dir].push_back(ProbParm::partVel[dir]);
+          host_real_attribs[PeleC::pstateT].push_back(T_ref);
+          host_real_attribs[PeleC::pstateDia].push_back(part_dia);
+          host_real_attribs[PeleC::pstateRho].push_back(part_rho);
+          host_real_attribs[PeleC::pstateY].push_back(1.);
+          for (int spf = 1; spf != SPRAY_FUEL_NUM; ++spf)
+            host_real_attribs[PeleC::pstateY+spf].push_back(0.);
+#else
+          for (int dir = 0; dir != AMREX_SPACEDIM; ++dir)
+            p.rdata(PeleC::pstateVel+dir) = ProbParm::partVel[dir];
+          p.rdata(PeleC::pstateT) = T_ref; // temperature
+          p.rdata(PeleC::pstateDia) = part_dia; // diameter
+          p.rdata(PeleC::pstateRho) = part_rho; // liquid fuel density
+          for (int sp = 0; sp != SPRAY_FUEL_NUM; ++sp)
+            p.rdata(PeleC::pstateY + sp) = 0.;
+          p.rdata(PeleC::pstateY) = 1.; // Only use the first fuel species
+#endif
+          host_particles.push_back(p);
+#if AMREX_SPACEDIM == 3
+          part_loc[2] += dx_part[2];
+        }
+#endif
+        part_loc[1] += dx_part[1];
+      }
+      part_loc[0] += dx_part[0];
+    }
+    auto& particles = GetParticles(lev);
+    auto& particle_tile = particles[std::make_pair(mfi.index(), mfi.LocalTileIndex())];
+    auto old_size = particle_tile.GetArrayOfStructs().size();
+    auto new_size = old_size + host_particles.size();
+    particle_tile.resize(new_size);
+
+    // Copy the AoS part of the host particles to the GPU
+    Gpu::copy(Gpu::hostToDevice, host_particles.begin(), host_particles.end(),
+              particle_tile.GetArrayOfStructs().begin() + old_size);
+#ifdef USE_SPRAY_SOA
+    for (int i = 0; i != NAR_SPR; ++i) {
+      Gpu::copy(Gpu::hostToDevice, host_real_attribs[i].begin(), host_real_attribs[i].end(),
+                particle_tile.GetStructOfArrays().GetRealData(i).begin() + old_size);
+    }
+#endif
+  }
+  Redistribute();
+}
---
+++ Exec/SprayTests/timing_test/prob.cpp
@@ -0,0 +1,78 @@
+#include "prob.H"
+
+namespace ProbParm {
+AMREX_GPU_DEVICE_MANAGED amrex::Real mach = 0.1;
+AMREX_GPU_DEVICE_MANAGED amrex::Real p0 = 1.013e6; // [erg cm^-3]
+AMREX_GPU_DEVICE_MANAGED amrex::Real T0 = 1000.0;
+AMREX_GPU_DEVICE_MANAGED amrex::Real v0 = 0.0;
+AMREX_GPU_DEVICE_MANAGED amrex::Real Y_O2 = 0.233;
+AMREX_GPU_DEVICE_MANAGED amrex::Real Y_N2 = 0.767;
+AMREX_GPU_DEVICE_MANAGED amrex::IntVect partNum = amrex::IntVect(AMREX_D_DECL(100, 100, 100));
+AMREX_GPU_DEVICE_MANAGED amrex::Real partTemp = 300.;
+AMREX_GPU_DEVICE_MANAGED amrex::Real partRho = 1.;
+AMREX_GPU_DEVICE_MANAGED amrex::Real partDia = 1.E-3;
+AMREX_GPU_DEVICE_MANAGED amrex::RealVect partVel = amrex::RealVect(AMREX_D_DECL(0., 0., 0.));
+} // namespace ProbParm
+
+void
+pc_prob_close()
+{
+}
+
+extern "C" {
+void
+amrex_probinit(
+  const int* init,
+  const int* name,
+  const int* namelen,
+  const amrex_real* problo,
+  const amrex_real* probhi)
+{
+  // Parse params
+  amrex::ParmParse pp("prob");
+  pp.query("mach", ProbParm::mach);
+  pp.query("ref_p", ProbParm::p0);
+  pp.query("ref_T", ProbParm::T0);
+  pp.query("init_O2", ProbParm::Y_O2);
+  pp.query("init_N2", ProbParm::Y_N2);
+  pp.query("num_particles", ProbParm::partNum);
+  std::array<amrex::Real, AMREX_SPACEDIM> pvel;
+  pp.query<amrex::Real>("part_vel", pvel);
+  for (int dir = 0; dir != AMREX_SPACEDIM; ++dir) ProbParm::partVel[dir] = pvel[dir];
+  pp.get("part_rho", ProbParm::partRho);
+  pp.get("part_dia", ProbParm::partDia);
+  pp.get("part_temp", ProbParm::partTemp);
+
+  // Initial density, velocity, and material properties
+  amrex::Real cs, cp;
+  amrex::Real massfrac[NUM_SPECIES] = {0.0};
+  massfrac[O2_ID] = ProbParm::Y_O2;
+  massfrac[N2_ID] = ProbParm::Y_N2;
+  amrex::Real wbar, gamma0;
+  EOS::TY2G(ProbParm::T0, massfrac, gamma0);
+  EOS::Y2WBAR(massfrac, wbar);
+  // Compute the speed of sound
+  cs = std::sqrt(ProbParm::T0 * gamma0 * EOS::RU / wbar);
+  ProbParm::v0 = ProbParm::mach * cs;
+
+  // Output IC
+  if (amrex::ParallelDescriptor::IOProcessor()) {
+    std::ofstream ofs("ic.txt", std::ofstream::out);
+    amrex::Print(ofs) << "number of particles: " << ProbParm::partNum[0];
+    for (int dir = 1; dir != AMREX_SPACEDIM; ++dir)
+      amrex::Print(ofs) << ", " << ProbParm::partNum[dir];
+    amrex::Print(ofs) << std::endl;
+    amrex::Print(ofs) << "particle velocity: " << ProbParm::partVel[0];
+    for (int dir = 1; dir != AMREX_SPACEDIM; ++dir)
+      amrex::Print(ofs) << ", " << ProbParm::partVel[dir];
+    amrex::Print(ofs) << std::endl;
+    amrex::Print(ofs) << "p0: " << ProbParm::p0 << std::endl;
+    amrex::Print(ofs) << "cs: " << cs << std::endl;
+    amrex::Print(ofs) << "U: " << ProbParm::v0 << std::endl;
+    amrex::Print(ofs) << "particle diameter: " << ProbParm::partDia << std::endl;
+    amrex::Print(ofs) << "particle density: " << ProbParm::partRho << std::endl;
+    ofs.close();
+  }
+}
+}
+
