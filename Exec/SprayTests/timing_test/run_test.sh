ee89a81856b609f6352f5c2807ff98b46f7bfd2d
Landon Owen <ldowen@sandia.gov>
2020-09-08 09:57:29 -0700

    Changed functionality so to consolidate temp MF and SumBoundary operations over all particle types, changed lev to level

--- Exec/SprayTests/timing_test/run_test.sh
+++ Exec/SprayTests/timing_test/run_test.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
+# Tells script to stop running as soon as an error occurs
+set -e
 EXEC="./PeleC2d.gnu.TPROF.MPI.ex"
 TPD="output_files"
+# For name of grid input file
+# Determines box sizes on refined levels
 gridsizes=(32 64 128)
+# Box sizes for coarse level
 bsize=(32 64 256)
 mkdir -p ${TPD}
 
 GRIDLOCS="two_d_gridfiles"
-#GRIDLOCS="shifted_refine"
 # Number of iterations, should have 6 digits
 NUM_ITER=000005
+INPUT_FILE=inputs_2d
 
 for gsi in "${!gridsizes[@]}"; do
     gs=${gridsizes[$gsi]}
     outloc=${TPD}/${gs}_BOX
     gridfile=${GRIDLOCS}/gridfile_${gs}
     mkdir -p $outloc
-    mpiexec -np 4 ${EXEC} inputs_2d \
-        amr.initial_grid_file = $gridfile \
-        amr.plot_file = $outloc/plt \
-        amr.blocking_factor = ${bsize[$gsi]} \
-        amr.max_grid_size = ${bsize[$gsi]} \
-        max_step = ${NUM_ITER} \
-        amr.plot_int = ${NUM_ITER}
+    mpiexec -np 4 ${EXEC} $INPUT_FILE \
+            amr.plot_file = $outloc/plt \
+            amr.blocking_factor = ${bsize[$gsi]} \
+            amr.max_grid_size = ${bsize[$gsi]} \
+            max_step = ${NUM_ITER} \
+            amr.plot_int = ${NUM_ITER} \
+            amr.initial_grid_file = $gridfile
 done
 cd ${TPD}
 FCOMP_EXEC=/Users/ldowen/Codes/amrex/Tools/Plotfile/fcompare.gnu.ex
