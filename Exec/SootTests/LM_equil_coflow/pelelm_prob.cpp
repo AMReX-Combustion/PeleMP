d9b3f3f42370dca8c08621c511c3131859f184b7
Landon Owen <ldowen@sandia.gov>
2021-07-29 10:54:57 -0700

    Include optimizations from GPU hackathon

--- Exec/SootTests/LM_equil_coflow/pelelm_prob.cpp
+++ Exec/SootTests/LM_equil_coflow/pelelm_prob.cpp
@@ -41,13 +41,16 @@ read_data(const std::string& myfile)
   int Ycol = -1;
   int Tcol = -1;
   int Zcol = -1;
+  int sootcol = -1;
   for (int i = 0; i < variable_count; i++) {
     std::string cur_name = var_names[i];
     if (cur_name == "Z" || cur_name == "z")
       Zcol = i;
     else if (cur_name == "T" || cur_name == "temp")
       Tcol = i;
-    else if (cur_name != spec_names[i - 2]) {
+    else if (cur_name == "soot_N")
+      sootcol = i;
+    else if (cur_name != spec_names[i - 2] && i < sootcol) {
       Print() << cur_name << " " << spec_names[i-2] << std::endl;
       amrex::Abort("Variables do not match");
     }
@@ -56,12 +59,16 @@ read_data(const std::string& myfile)
   while (std::getline(iss, remaininglines)) {
     line_count++;
   }
-  if (Zcol != 0 || Tcol != 1) amrex::Abort("Columns must be ordered Z, T, Y");
+  bool read_soot = false;
+  if (sootcol > -1) read_soot = true;
+  if (Zcol != 0 || Tcol != 1)
+    amrex::Abort("Columns must be ordered Z, T, Y");
   amrex::Print() << line_count << " lines found in data file" << std::endl;
   PeleLM::prob_parm->N_equil = line_count;
   std::vector<amrex::Real> T_vals(line_count);
   std::vector<amrex::Real> Z_vals(line_count);
   std::vector<amrex::Real> Y_vals(line_count * NUM_SPECIES);
+  std::vector<amrex::Real> soot_vals(line_count * (NUM_SOOT_MOMENTS + 1), 0.);
 
   iss.clear();
   iss.seekg(0, std::ios::beg);
@@ -73,6 +80,11 @@ read_data(const std::string& myfile)
     sinput >> T_vals[i];
     for (unsigned int j = 0; j < NUM_SPECIES; j++) {
       sinput >> Y_vals[j * line_count + i];
+    }
+    if (read_soot) {
+      for (unsigned int j = 0; j < NUM_SOOT_MOMENTS + 1; j++) {
+        sinput >> soot_vals[j * line_count + i];
+      }
     }
   }
 
@@ -92,15 +104,41 @@ read_data(const std::string& myfile)
     }
   }
 
+  amrex::Real initmoments[NUM_SOOT_MOMENTS + 1];
+  SootData* sd = nullptr;
+  SootConst sc;
+  bool use_soot = false;
+  if (PeleLM::soot_model) {
+    sd = PeleLM::soot_model->getSootData();
+    use_soot = true;
+  }
+  if (!read_soot && use_soot) {
+    sd->initialSmallMomVals(initmoments);
+  }
   PeleLM::prob_parm->Z_equil = (amrex::Real *) The_Arena()->alloc(line_count * sizeof(amrex::Real));
   PeleLM::prob_parm->T_equil = (amrex::Real *) The_Arena()->alloc(line_count * sizeof(amrex::Real));
   PeleLM::prob_parm->Y_equil = (amrex::Real *) The_Arena()->alloc(line_count * NUM_SPECIES * sizeof(amrex::Real));
+  PeleLM::prob_parm->soot_equil =
+    (amrex::Real *) The_Arena()->alloc(line_count * (NUM_SOOT_MOMENTS + 1) * sizeof(amrex::Real));
   for (unsigned int i = 0; i < line_count; i++) {
     PeleLM::prob_parm->Z_equil[i] = Z_vals[i];
     PeleLM::prob_parm->T_equil[i] = T_vals[i];
     for (unsigned int j = 0; j < NUM_SPECIES; ++j) {
       const int k = j * line_count + i;
       PeleLM::prob_parm->Y_equil[k] = Y_vals[k];
+    }
+    if (use_soot) {
+      // Assume soot values are given in CGS, must convert to SI
+      for (unsigned int j = 0; j < NUM_SOOT_MOMENTS + 1; ++j) {
+        const int k = j * line_count + i;
+        amrex::Real unitexp = 3. * sc.MomOrderV[j] + 2. * sc.MomOrderS[j];
+        amrex::Real unit_conv = std::pow(sc.len_conv, 3. - unitexp);
+        if (read_soot) {
+          PeleLM::prob_parm->soot_equil[k] = soot_vals[k] * unit_conv;
+        } else {
+          PeleLM::prob_parm->soot_equil[k] = initmoments[j];
+        }
+      }
     }
   }
 }
@@ -116,6 +154,10 @@ amrex_probinit(
 {
   // Parse params
   amrex::ParmParse pp("prob");
+  // This is the time to start transitioning to equilibrium inlet conditions
+  pp.query("equil_start_time", PeleLM::prob_parm->equil_start_time);
+  // This is the time inlet conditions should be all equilibrium
+  pp.query("equil_end_time", PeleLM::prob_parm->equil_end_time);
   pp.query("P_mean", PeleLM::prob_parm->P_mean);
   pp.query("fuel_temp", PeleLM::prob_parm->fuel_T);
   pp.query("oxid_temp", PeleLM::prob_parm->oxid_T);
@@ -157,11 +199,11 @@ amrex_probinit(
   pp.get("data_file", filename);
   read_data(filename);
 
-  amrex::Real moments[NUM_SOOT_MOMENTS + 1];
-  SootData* const sd = PeleLM::soot_model->getSootData();
-  sd->initialSmallMomVals(moments);
-  for (int n = 0; n < NUM_SOOT_MOMENTS + 1; ++n) {
-    PeleLM::prob_parm->soot_vals[n] = moments[n];
-  }
+  // amrex::Real moments[NUM_SOOT_MOMENTS + 1];
+  // SootData* const sd = PeleLM::soot_model->getSootData();
+  // sd->initialSmallMomVals(moments);
+  // for (int n = 0; n < NUM_SOOT_MOMENTS + 1; ++n) {
+  //   PeleLM::prob_parm->soot_vals[n] = moments[n];
+  // }
 }
 }
