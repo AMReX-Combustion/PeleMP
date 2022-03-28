3b1711ce579f2a6c16f35f2af19b338f799651f2
Landon Owen <ldowen@sandia.gov>
2022-03-28 09:06:01 -0700

    Added Abramzon test for PeleLMeX

---
+++ Exec/SprayTests/xLMtest/abramzon_test/SprayParticlesInitInsert.cpp
@@ -0,0 +1,23 @@
+
+#include "SprayParticles.H"
+#include <AMReX_Particles.H>
+#include <pelelm_prob.H>
+
+using namespace amrex;
+
+bool
+SprayParticleContainer::injectParticles(
+  Real time,
+  Real dt,
+  int nstep,
+  int lev,
+  int finest_level,
+  ProbParm const& prob_parm)
+{
+  return false;
+}
+
+void
+SprayParticleContainer::InitSprayParticles(ProbParm const& prob_parm)
+{
+}
