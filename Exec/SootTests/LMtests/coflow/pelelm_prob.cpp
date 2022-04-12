095668381d82bfac6493fd403cb0aabdbf6a16e8
Landon Owen <ldowen@sandia.gov>
2022-04-12 13:36:24 -0700

    Changed SPRAY_PELE_LM to PELELM_USE_SPRAY, changed SOOT_PELE_LM to PELELM_USE_SOOT

--- Exec/SootTests/LMtests/coflow/pelelm_prob.cpp
+++ Exec/SootTests/LMtests/coflow/pelelm_prob.cpp
@@ -54,13 +54,11 @@ amrex_probinit(
   if (PeleLM::prob_parm->ign_indx < 0) {
     amrex::Abort("Ignition species not found from reaction data");
   }
-#ifdef SOOT_MODEL
   amrex::Real moments[NUM_SOOT_MOMENTS + 1];
   SootData* const sd = PeleLM::soot_model->getSootData();
   sd->initialSmallMomVals(moments);
   for (int n = 0; n < NUM_SOOT_MOMENTS + 1; ++n) {
     PeleLM::prob_parm->soot_vals[n] = moments[n];
   }
-#endif
 }
 }
