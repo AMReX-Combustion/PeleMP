549f8cd70289f7e85e531658055d017f51be0424
Landon Owen <ldowen@sandia.gov>
2023-05-31 17:25:40 -0700

    Added functionality to read in ECN ROI files, fixed dt estimate to accomodate max jet velocity, fixed some clang-tidy issues

--- Exec/SprayTests/PeleC/jet_spray/prob.cpp
+++ Exec/SprayTests/PeleC/jet_spray/prob.cpp
@@ -1,52 +1,5 @@
 #include "prob.H"
 
-std::string
-read_inject_file(std::ifstream& in)
-{
-  return static_cast<std::stringstream const&>(
-           std::stringstream() << in.rdbuf())
-    .str();
-}
-
-void
-read_inject(const std::string myfile)
-{
-  std::string firstline, remaininglines;
-  int line_count;
-
-  std::ifstream infile(myfile);
-  const std::string memfile = read_inject_file(infile);
-  if (!amrex::FileSystem::Exists(myfile)) {
-    amrex::Abort("ROI file does not exists");
-  }
-  infile.close();
-  std::istringstream iss(memfile);
-
-  std::getline(iss, firstline);
-  line_count = 0;
-  while (std::getline(iss, remaininglines)) {
-    line_count++;
-  }
-  amrex::Print() << line_count << " data lines found in inject file"
-                 << std::endl;
-
-  PeleC::prob_parm_host->inject_N = line_count;
-  PeleC::prob_parm_host->inject_time.resize(PeleC::prob_parm_host->inject_N);
-  PeleC::prob_parm_host->inject_mass.resize(PeleC::prob_parm_host->inject_N);
-  PeleC::prob_parm_host->inject_vel.resize(PeleC::prob_parm_host->inject_N);
-
-  iss.clear();
-  iss.seekg(0, std::ios::beg);
-  std::getline(iss, firstline);
-  for (int i = 0; i < PeleC::prob_parm_host->inject_N; i++) {
-    std::getline(iss, remaininglines);
-    std::istringstream sinput(remaininglines);
-    sinput >> PeleC::prob_parm_host->inject_time[i];
-    sinput >> PeleC::prob_parm_host->inject_mass[i];
-    sinput >> PeleC::prob_parm_host->inject_vel[i];
-  }
-}
-
 extern "C" {
 void
 amrex_probinit(
@@ -58,7 +11,6 @@ amrex_probinit(
 {
   // Parse params
   amrex::ParmParse pp("prob");
-  pp.query("inject_file", PeleC::prob_parm_host->input_file);
   pp.query("init_v", PeleC::h_prob_parm_device->v0);
   pp.get("ref_p", PeleC::h_prob_parm_device->p0);
   pp.get("ref_T", PeleC::h_prob_parm_device->T0);
@@ -78,10 +30,6 @@ amrex_probinit(
     PeleC::h_prob_parm_device->rho0, PeleC::h_prob_parm_device->T0, massfrac,
     cs);
   eos.TY2Cp(PeleC::h_prob_parm_device->T0, massfrac, cp);
-  // Read injection ROI file if specified
-  if (PeleC::prob_parm_host->input_file != "") {
-    read_inject(PeleC::prob_parm_host->input_file);
-  }
 }
 }
 
