161e40ea587a6340592b9125f98a9aaaa498ce1d
Landon Owen <Lndnwn9@gmail.com>
2023-06-30 15:29:02 -0700

    Clang-format fixes

--- Source/PP_Spray/SprayParticles.cpp
+++ Source/PP_Spray/SprayParticles.cpp
@@ -305,9 +305,9 @@ SprayParticleContainer::updateParticles(
 #ifdef AMREX_USE_EB
             if (eb_in_box) {
               do_fe_interp = eb_interp(
-                p, isVirt, ijkc, ijk, dx, dxi, lx, plo, bflags, flags_array,
-                ccent_fab, bcent_fab, bnorm_fab, volfrac_fab,
-                fdat->min_eb_vfrac, indx_array.data(), weights.data());
+                p, ijkc, ijk, dx, dxi, lx, plo, bflags, flags_array, ccent_fab,
+                bcent_fab, bnorm_fab, volfrac_fab, fdat->min_eb_vfrac,
+                indx_array.data(), weights.data());
             } else
 #endif
             {
@@ -374,12 +374,12 @@ SprayParticleContainer::updateParticles(
                 } else {
                   // Next reflect particles off BC or EB walls if necessary
                   impose_wall(
-                    p, dx, plo, phi, bndry_lo, bndry_hi, bflags, eb_in_box,
+                    p, dx, plo, phi, bflags, eb_in_box,
 #ifdef AMREX_USE_EB
                     flags_array, bcent_fab, bnorm_fab, volfrac_fab,
-                    fdat->min_eb_vfrac,
+                    fdat->min_eb_vfrac, ijkc_prev,
 #endif
-                    ijkc, ijkc_prev);
+                    ijkc);
                   lx = (p.pos() - plo) * dxi + 0.5;
                   ijk = lx.floor();
                   lxc = (p.pos() - plo) * dxi;
